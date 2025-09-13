f4381a333fb819c1e79f9e7c3abe9c426ce78063
Abraham Haile Araya <107077258+abrshz@users.noreply.github.com>
2025-09-14 00:00:21 +0300

    Set up the vote using formAction

--- frontend/src/components/Opinion.jsx
+++ frontend/src/components/Opinion.jsx
@@ -1,4 +1,17 @@
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
+
+  function upvoteAction() {
+    console.log("Up Vote");
+
+  }
+
+  function downvoteAction() {
+    console.log("Down vote ");
+
+  }
+
+
+
   return (
     <article>
       <header>
@@ -7,7 +20,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
       </header>
       <p>{body}</p>
       <form className="votes">
-        <button>
+        <button formAction={upvoteAction}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -27,7 +40,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
 
         <span>{votes}</span>
 
-        <button>
+        <button formAction={downvoteAction} >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
